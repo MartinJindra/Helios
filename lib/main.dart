bcabb675cf89c7c94d010ee7a0e8e0348adf20e8
MartinJindra <martin@mjindra.eu>
2023-07-22 18:04:36 +0200

    rm args

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,4 @@ import 'package:helios/view/mainview.dart';
 
 void main() {
   runApp(MainView());
-  //ArgResults parsedArgs = Parser.parse(args);
-  //if (parsedArgs.arguments.isNotEmpty) Runner.run(parsedArgs);
 }
